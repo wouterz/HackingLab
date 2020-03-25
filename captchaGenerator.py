0ca15fa6fd2c29c8647e68d69221511ae27e4079
Marco Houtman <m.houtman@student.tudelft.nl>
2020-03-26 00:32:15 +0100

    change number of generated images to 1000

--- captchaGenerator.py
+++ captchaGenerator.py
@@ -12,7 +12,7 @@ except:
 alphabet_lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 
 for i in range(26):
-    for j in range(1):   
+    for j in range(1000):   
         image_captcha = ImageCaptcha(width=size, height=size)
         image = image_captcha.generate_image(alphabet_lowercase[i])
         #image.show()
