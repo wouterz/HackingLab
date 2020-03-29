58cfbe570185b011abe74ec77fa7459425408e07
Matteo Medioli <31959430+matteomedioli@users.noreply.github.com>
2020-03-29 16:58:48 +0200

    HRSAdvModel: construct _hrs in make_image_model

--- AdvModel.py
+++ AdvModel.py
@@ -7,6 +7,9 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.layers import Layer
 
+from keras_utils import construct_hrs_model
+from block_split_config import get_split
+
 
 class AdvLayer(Layer):
 
@@ -81,6 +84,99 @@ class AdvModel:
                 shape=(self.image_size, self.image_size, 3)
             ), layers=tf.keras.layers)
             inception.trainable = False
+        return inception
+
+    def build_model(self):
+        # Adv Layer
+        inputs = Input(shape=(self.center_size, self.center_size, self.channels))
+        al = AdvLayer(image_size=self.image_size, center_size=self.center_size, channels=self.channels)(inputs)
+
+
+        # Combine layers
+        outputs = self.image_model(al)
+
+        model = Model(inputs=[inputs],
+                      outputs=[outputs])
+        self.model = model
+        lr_metric = self.get_lr_metric(self.optimizer)
+
+        model.compile(optimizer=self.optimizer,
+                      loss=['categorical_crossentropy'],
+                      metrics=['accuracy', lr_metric])
+
+    def fit_model(self, x_train, y_train, x_valid, y_valid, weights, save_path=""):
+        savepath = "%sweights.{epoch:02d}-{loss:.2f}.hdf5" % save_path
+
+        cbks = [
+            tf.keras.callbacks.LearningRateScheduler(schedule=lambda epoch: self.step_decay(epoch=epoch), verbose=1),
+            tf.keras.callbacks.ModelCheckpoint(filepath=savepath, verbose=0,
+                                               save_best_only=True, save_weights_only=False, mode='auto', period=5)]
+
+        history = self.model.fit(x=x_train, y=y_train, validation_data=(x_valid, y_valid),
+                                 epochs=self.epochs,
+                                 initial_epoch = self.previousEpoch,
+                                 batch_size=self.batch_size, callbacks=cbks, class_weight=weights, shuffle=True)
+        return history
+
+    def continue_model(self, current_epoch, weights, x_train, y_train, x_valid, y_valid, weigths, save_path=""):
+        self.model.load_weights(weights)
+        self.previousEpoch = current_epoch
+        self.fit_model(x_train, y_train, x_valid, y_valid, weights, save_path)
+
+    def step_decay(self, epoch):
+        lr = self.adam_learn_rate
+        drop = self.adam_decay
+        lrate = float(lr * math.pow(drop, epoch // self.step))
+        return lrate
+
+    def get_model(self):
+        return self.model
+
+    def _loss_tensor(self, y_true, y_pred):
+        cross_entropy_loss = tf.nn.softmax_cross_entropy_with_logits(labels=y_true, logits=y_pred)
+        reg_loss = 2e-6 * tf.nn.l2_loss(self.model.get_layer('adv_layer').adv_weights)
+        loss = cross_entropy_loss + reg_loss
+        return loss
+
+    # def accuracy(self, y_true, y_pred):
+    #     correct_predictions = tf.equal(tf.argmax(y_true, 1), tf.argmax(y_pred, 1))
+    #     accuracy = tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
+    #     return accuracy
+
+    def get_lr_metric(self, optimizer):
+        def lr(y_true, y_pred):
+            return optimizer.lr
+
+        return lr
+   
+
+class HRSAdvModel:
+
+    def __init__(self, epochs, batch_size, center_size, image_size, channels,
+                 adam_learn_rate, adam_decay, step, model_name):
+        self.previousEpoch = 0
+        self.step = step
+        self.epochs = epochs
+        self.batch_size = batch_size
+        self.center_size = center_size
+        self.image_size = image_size
+        self.channels = channels
+        self.adam_learn_rate = adam_learn_rate
+        self.adam_decay = adam_decay
+
+        self.optimizer = Adam(lr=adam_learn_rate)
+
+        self.image_model = self.make_image_model(model_name)
+        self.build_model()
+
+    def make_image_model(self, model_name, model_indicator):
+        inception = []
+        if (model_name == "inception_v3"):
+            blocks_definition = get_split(split, 'IMAGENET')
+            model = construct_hrs_model(dataset='IMAGENET', model_indicator=model_indicator, blocks_definition=blocks_definition)
+            # set layer untrainable
+            for layer in model.layers:
+                layer.trainable = False
         return inception
 
     def build_model(self):
