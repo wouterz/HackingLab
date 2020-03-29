7a72fd60515d8a356d826bd64b438fd2598611fd
Matteo Medioli <31959430+matteomedioli@users.noreply.github.com>
2020-03-29 16:51:36 +0200

    Update defend_adversarial_reprogramming.py

--- defend_adversarial_reprogramming.py
+++ defend_adversarial_reprogramming.py
@@ -21,53 +21,7 @@ def defend_adversarial_reprogramming(model_indicator, split, epochs):
     # get MNIST data
     [X_train, X_test, Y_train, Y_test] = get_data(dataset='MNIST', scale1=True, one_hot=False, percentage=0.01)
 
-    # input transfer model
-    input_transfer = Sequential()
-    input_transfer.add(ZeroPadding2D(padding=3, input_shape=(28, 28, 1)))
-    input_transfer.add(LocallyConnected2D(3, (3, 3), activation='relu'))
-    input_transfer.add(Activation('tanh'))
-
-    # get block definitions
-    blocks_definition = get_split(split, 'IMAGENET')
-
-    # target model to reprogram
-    keras.backend.set_learning_phase(0)
-    model = construct_hrs_model(dataset='IMAGENET', model_indicator=model_indicator, blocks_definition=blocks_definition)
-    # set layer untrainable
-    for layer in model.layers:
-        layer.trainable = False
-
-    # overall model
-    output = model(input_transfer.output)
-    adv_model = Model(input_transfer.input, output)
-
-    # optimizer and loss
-    sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-    def fn(correct, predicted):
-        return tf.nn.softmax_cross_entropy_with_logits(labels=correct,
-                                                       logits=predicted)
-
-    # compile
-    adv_model.compile(loss=fn,
-                      optimizer=sgd,
-                      metrics=['accuracy'])
-
-    # training the input transfer
-    hist = adv_model.fit(X_train, Y_train,
-                         batch_size=128,
-                         validation_data=(X_test, Y_test),
-                         nb_epoch=epochs,
-                         shuffle=True)
-
-    # save training history
-    train_acc = hist.history['acc']
-    test_acc = hist.history['val_acc']
-
-    np.save(save_dir + 'hist.npy', np.array(hist.history))
-    print('Perform adversarial reprogramming to model %s' % model_indicator)
-    print('Reprogramming Train Acc. after %d epochs of training is %.4f' % (epochs, train_acc[-1]))
-    print('Reprogramming Test Acc. after %d epochs of training is %.4f' % (epochs, test_acc[-1]))
-
+    #TODO: Add HRS_ADVModel
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
