0162027dc4603a7270d2a928f1f51e1aa059b0db
Marco Houtman <m.houtman@student.tudelft.nl>
2020-03-30 02:35:23 +0200

    Add extra functionality captcha generator

--- captchaGenerator.py
+++ captchaGenerator.py
@@ -1,20 +1,39 @@
 from captcha.image import ImageCaptcha
 import matplotlib.pyplot as plt
+import argparse
 import os
 
+parser = argparse.ArgumentParser()
+parser.add_argument('choice', choices=['uppercase', 'lowercase', 'digits'], help='uppercase, lowercase or digits')
+parser.add_argument('--images_per_class', type=int, help="How many images should be generated per class", default=10)
+parser.add_argument('--size', type=int, help="Size of the captcha image", default=60)
+args = parser.parse_args()
+choice = args.choice
+size = args.size
+number_of_images = args.images_per_class
 
-size = 60
+def createCaptcha(items, directory, size, number_of_images):
+    savedir = "./images/captcha_{}_{}".format(directory, size)
+    try:
+        os.stat(savedir)
+    except:
+        os.makedirs(savedir)
 
-try:
-    os.stat("./images/captcha_{}".format(size))
-except:
-    os.makedirs("./images/captcha_{}".format(size))
-alphabet_lowercase = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    for i in range(len(items)):
+        for j in range(number_of_images):   
+            image_captcha = ImageCaptcha(width=size, height=size)
+            image = image_captcha.generate_image(items[i])
+            #image.show()
+            location = '{}/{}_{}_{}.png'.format(savedir,directory,items[i],j)
+            if(not os.path.exists(location)):
+                image.save(location)
+items = []
+#choice = ""
+choices = {
+    "lowercase" : ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'],
+    "uppercase" : list('ABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+    "digits" : list("0123456789")
+}
 
-for i in range(26):
-    for j in range(1000):   
-        image_captcha = ImageCaptcha(width=size, height=size)
-        image = image_captcha.generate_image(alphabet_lowercase[i])
-        #image.show()
-        image.save('./images/captcha_{}/Letter_{}_{}.png'.format(size,alphabet_lowercase[i],j))
+createCaptcha(choices[choice], choice, size, number_of_images)
 
